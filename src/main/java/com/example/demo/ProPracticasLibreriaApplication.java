6361d74a14699534015e300286742fca9822a680
cesar prerez santana garcia <cesardim45@gmail.com>
2023-03-07 12:42:36 +0100

    Commit Inicial

---
+++ src/main/java/com/example/demo/ProPracticasLibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class ProPracticasLibreriaApplication {
+
+	public static void main(String[] args) {
+	}
+
+	public static void mostrarMensaje() {
+		for (int i = 0; i < 20; i++) {
+			System.out.println("Hola mundo");
+		}
+	}
+}
